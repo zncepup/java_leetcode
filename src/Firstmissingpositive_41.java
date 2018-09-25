8f53e14c83ba7c92ef9ae0f1021e12e78b62f00f
zncepup <1138139812@qq.com>
2018-09-25 23:19:20 +0800

    First Missing Positive  041 leetcode

---
+++ src/Firstmissingpositive_41.java
@@ -0,0 +1,45 @@
+public class Firstmissingpositive_41 {
+}
+
+///copy from https://leetcode.com/problems/first-missing-positive/discuss/17073/Share-my-O(n)-time-O(1)-space-solution
+//鸽巢原理
+//public int firstMissingPositive(int[] A) {
+//    int n=A.length;
+//    if(n==0)
+//        return 1;
+//    int k=partition(A)+1;
+//    int temp=0;
+//    int first_missing_Index=k;
+//    for(int i=0;i<k;i++){
+//        temp=Math.abs(A[i]);
+//        if(temp<=k)
+//            A[temp-1]=(A[temp-1]<0)?A[temp-1]:-A[temp-1];
+//    }
+//    for(int i=0;i<k;i++){
+//        if(A[i]>0){
+//            first_missing_Index=i;
+//            break;
+//        }
+//    }
+//    return first_missing_Index+1;
+//}
+//
+//public int partition(int[] A){
+//    int n=A.length;
+//    int q=-1;
+//    for(int i=0;i<n;i++){
+//        if(A[i]>0){
+//            q++;
+//            swap(A,q,i);
+//        }
+//    }
+//    return q;
+//}
+//
+//public void swap(int[] A, int i, int j){
+//    if(i!=j){
+//        A[i]^=A[j];
+//        A[j]^=A[i];
+//        A[i]^=A[j];
+//    }
+//}
